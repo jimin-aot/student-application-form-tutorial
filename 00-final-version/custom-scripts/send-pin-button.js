776013d2865aae288582ca70eb67370116c6902c
jimin-aot <jimin.byun@aot-technologies.com>
2024-01-08 15:36:12 -0800

    Updated JS for Send Pin button

---
+++ 00-final-version/custom-scripts/send-pin-button.js
@@ -0,0 +1,67 @@
+async function startEmailVerificationProcess() {
+  resetPinFields();
+
+  console.log('Send PIN clicked');
+  const processDefinitionKey = 'vsb-email-verification';
+  const tenantId = 'jiminb';
+  const bpmApiUrl = localStorage.getItem('bpmApiUrl');
+  const authToken = localStorage.getItem('authToken');
+
+  if (!bpmApiUrl) {
+    console.error('BPM API URL not found in localStorage.');
+    return;
+  }
+
+  if (!authToken) {
+    console.error('Authorization token is not available in localStorage.');
+    return;
+  }
+
+  const url = `${bpmApiUrl}/process-definition/key/${processDefinitionKey}/start?tenantId=${tenantId}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify({
+        variables: {
+          email: { value: data.email },
+          applicationId: { value: data.applicationId || 25 }, // fallback to random number if data.applicationId is not available
+          formName: { value: 'jmbi-studentapplication' },
+          tenantKey: { value: 'jmbi' },
+        },
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    console.log('Process started successfully:', responseData);
+    localStorage.setItem('processInstanceId', responseData.id);
+    data.pinVerificationStatus = 'pinSent';
+
+    // instance.root.redraw();
+    if (form.components[1].components[2]) {
+      form.components[1].components[2].visible = true;
+      form.components[1].components[2].redraw();
+    }
+  } catch (error) {
+    console.error('Error during the process operation:', error.message);
+  }
+}
+
+// Reset function to clear PIN fields and any validation messages
+function resetPinFields() {
+  data.userInputPin = '';
+  data.pinVerificationStatus = '';
+  if (instance && instance.root) {
+    instance.root.setPristine(true);
+  }
+}
+
+startEmailVerificationProcess();
